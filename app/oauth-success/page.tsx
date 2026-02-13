96d74ebb128fe261c6a30f711b197285a2bb1419
Raja2703 <srajasep27@gmail.com>
2026-02-13 16:16:34 +0530

    console added

--- app/oauth-success/page.tsx
+++ app/oauth-success/page.tsx
@@ -33,6 +33,7 @@ export default function OAuthSuccess() {
   // Step 2: redirect after profile loads
   useEffect(() => {
     if (!ready) return;
+    console.log(userProfile)
 
     if (isError) {
       toast.error("Failed to load profile");
